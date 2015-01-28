080c8be76012e4864c4b355ecd31dc6a5c4862c1
Daniel Greenfeld <danny@eventbrite.com>
2015-01-28 15:49:22 -0800

    Added many comments to the app as well as the .env file

--- app.py
+++ app.py
@@ -1,28 +1,29 @@
 import os
-# import traceback
 
+import eventbrite
 from flask import Flask, render_template, request
 import pusher
-import eventbrite
 
+# We fetch our constants by taking them from environment variables
+#   defined in the .env file.
+EVENTBRITE_EVENT_ID = os.environ['EVENTBRITE_EVENT_ID']
+EVENTBRITE_OAUTH_TOKEN= os.environ['EVENTBRITE_OAUTH_TOKEN']
 PUSHER_APP_ID = os.environ['PUSHER_APP_ID']
 PUSHER_KEY = os.environ['PUSHER_KEY']
 PUSHER_SECRET = os.environ['PUSHER_SECRET']
-EVENTBRITE_EVENT_ID = os.environ['EVENTBRITE_EVENT_ID']
-EVENTBRITE_OAUTH_TOKEN= os.environ['EVENTBRITE_OAUTH_TOKEN']
 
-API_URL = "https://www.eventbriteapi.com/v3"
+# Instantiate the Eventbrite API client.
+eventbrite = eventbrite.Eventbrite(EVENTBRITE_OAUTH_TOKEN)
 
+# Instantiate the pusher object. This library is used to push actions
+#   to the browser when they occur.
 p = pusher.Pusher(app_id=PUSHER_APP_ID, key=PUSHER_KEY, secret=PUSHER_SECRET)
 
 app = Flask(__name__)
-app.debug = True
-
-eventbrite = eventbrite.Eventbrite(EVENTBRITE_OAUTH_TOKEN)
-
 
 @app.route('/')
 def index():
+    """ This is the display view. """
 
     # Get the event details
     event = eventbrite.get_event(EVENTBRITE_EVENT_ID)
@@ -30,19 +31,13 @@ def index():
     # Get the attendee list
     attendees = eventbrite.get_event_attendees(EVENTBRITE_EVENT_ID)
 
+    # Render our HTML.
     return render_template(
         'index.html',
         settings={'PUSHER_KEY': PUSHER_KEY},
         event=event,
         attendees=attendees
     )
-
-@app.route('/add-danny')
-def add_danny():
-    user = eventbrite.get_user()
-    p['webhooks'].trigger("User", user)
-    return "Danny added"
-
 
 @app.route('/webhook', methods=['POST'])
 def webhook():
@@ -51,23 +46,15 @@ def webhook():
         more information. They are sent as HTTP POSTS with the JSON mimetype
         specified in the header.
 
-        A sample webhook:
-            {
-              "api_url": "https://www.eventbriteapi.com/v3/orders/1234567890/",
-              "config": {
-                "endpoint_url": "https://myawesomeapp.com/webhook",
-                "insecure_ssl": "0"
-              }
-            }
     """
-    # Get the webhook data out of flask.
-    # hook_data = request.get_json()
 
-    # Use the API client to convert from a webhook to an API object
+    # Use the API client to convert from a webhook to an API object. An API
+    #   object is just a Python dict with some extra methods.
     api_object = eventbrite.webhook_to_object(request)
+
+    # Use pusher to add content to to the HTML page.
     p['webhooks'].trigger("Attendee", api_object)
     return ""
-
 
 
 if __name__ == '__main__':
